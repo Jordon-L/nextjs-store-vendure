1f702e8cd7d013d49f04327672f66e609998601f
Jordon-L <jordonlee1412@gmail.com>
2023-07-10 00:51:41 -0700

    contact

---
+++ app/contact/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+export const dynamic = "force-dynamic";
+import ProductGrid from "@/components/ProductGrid";
+
+export default function Page() {
+  return (
+    <div>
+      <div className="center flex-col p-6">
+        <p>Contact Page</p>
+      </div>
+    </div>
+  );
+}
