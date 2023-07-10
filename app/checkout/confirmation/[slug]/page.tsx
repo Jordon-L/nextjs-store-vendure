48894a713c4c70612143054a3751980bef281fe3
Jordon-L <jordonlee1412@gmail.com>
2023-07-10 00:51:47 -0700

    confirmation page

--- app/checkout/confirmation/[slug]/page.tsx
+++ app/checkout/confirmation/[slug]/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 export const dynamic = "force-dynamic";
+import CartList from "@/components/CartList";
+import CartSummary from "@/components/CartSummary";
 import { StripePayments } from "@/components/Stripe";
 import { gql, useMutation, useQuery } from "@apollo/client";
 import { useRouter, useSearchParams } from "next/navigation";
@@ -16,9 +18,9 @@ const query = gql`
 `;
 
 const orderQuery = gql`
-  query order {
-    orderByCode {
-      ...cartDetails
+  query order($code: String!) {
+    orderByCode(code: $code) {
+      ...CartDetails
       state
     }
   }
@@ -45,13 +47,41 @@ const stripeMutation = gql`
   }
 `;
 
-export default function Payment() {
-  const params = useSearchParams();
-  const router = useRouter();
-  const order = useQuery(orderQuery);
+export default function Confirmation({ params }: { params: { slug: string } }) {
+  console.log(params.slug)
+  const order = useQuery(orderQuery, {variables: {code: params.slug}});
   return (
     <div className="center flex-col p-6">
       <p className="text-2xl mb-4 font-semibold">Confirmation</p>
+      <section className="pt-8 py-4 w-full flex-col hidden lg:block">
+        {/* Summary */}
+        <div className="summary grow  ">
+          {!order.data?.orderByCode ? (
+            <CartSummary
+              subTotal={0}
+              taxes={null}
+              shipping={0}
+              totalWithTax={0}
+            ></CartSummary>
+          ) : (
+            <CartSummary
+              subTotal={order.data.orderByCode?.subTotal}
+              taxes={order.data.orderByCode.taxSummary}
+              shipping={order.data.orderByCode.shipping}
+              totalWithTax={order.data.orderByCode.totalWithTax}
+            ></CartSummary>
+          )}
+        </div>
+        {/* Cart */}
+        <div>
+          {/* Item 1 */}
+          {!order.data?.orderByCode ? (
+            <div className="h-[500px]">Empty</div>
+          ) : (
+            <CartList activeOrder={order.data?.orderByCode} />
+          )}
+        </div>
+      </section>
     </div>
   );
 }
